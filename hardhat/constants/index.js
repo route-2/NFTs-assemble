2ae5bc1e3d88798bfe027903ccafdd0ca37f2d33
Ruthu Rao <ruthurao@gmail.com>
2023-02-01 20:43:58 +0530

    Update index.js

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,6 +1,6 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x2331e4d94c7e91c3Abe36bB546F3b41c782e3D7A"
 
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/ "
+const METADATA_URL = ""
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
 
